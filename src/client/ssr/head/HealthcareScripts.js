05110bb940cbf66d237218cab7a61493767fde94
underpost <52893447+underpostnet@users.noreply.github.com>
2026-08-23 10:26:21 -0400

    Update HealthcareScripts.js

--- src/client/ssr/head/HealthcareScripts.js
+++ src/client/ssr/head/HealthcareScripts.js
@@ -4,7 +4,11 @@ SrrComponent = ({ ssrPath }) => html`
   <script type="text/javascript" src="${ssrPath}dist/d3/d3.min.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/peerjs/peerjs.min.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/rrule/rrule.min.js"></script>
+  <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/skeleton.css" />
+  <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/themes/classic/theme.css" />
+  <link rel="stylesheet" href="${ssrPath}dist/fullcalendar/themes/classic/palette.css" />
   <script type="text/javascript" src="${ssrPath}dist/fullcalendar/global.js"></script>
+  <script type="text/javascript" src="${ssrPath}dist/fullcalendar/themes/classic/global.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/@fullcalendar/rrule/global.js"></script>
   <script type="text/javascript" src="${ssrPath}dist/easymde/easymde.min.js"></script>
   <link rel="stylesheet" href="${ssrPath}dist/easymde/easymde.min.css" />
